b8a3abb4cebb43d6eef1109ac8edbc34148eddf0
MohamedFathy610 <147451248+MohamedFathy610@users.noreply.github.com>
2025-07-13 22:57:59 +0300

    Create Z. HardCompare.cpp

---
+++ Fuction/Z. HardCompare.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	long long a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (log(a) * b > log(c) * d)
+		cout << "YES";
+	else
+		cout << "NO";
+	
+	return 0;
+}
+ 
